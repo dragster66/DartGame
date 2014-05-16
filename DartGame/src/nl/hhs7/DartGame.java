ee0943e6481098978fac0bdd4fc53d17ce1e0485
Joris Dek <dragsster66@gmail.com>
2014-05-16 11:52:54 +0200

    Eerste commit

---
+++ DartGame/src/nl/hhs7/DartGame.java
@@ -0,0 +1,30 @@
+package nl.hhs7;
+
+public class DartGame {
+	private char HUMAN_PLAYER;
+	private char COM_PLAYER;
+	
+	
+	
+	private static int BOARD_SIZE = 20;
+	
+	public int getBoardSize()
+	{
+		return BOARD_SIZE;
+	}
+	
+	public void setPlayer()
+	{
+		this.HUMAN_PLAYER = HUMAN_PLAYER;
+	}
+	public char getPlayer()
+	{
+		return HUMAN_PLAYER;
+	}
+	
+	public DartGame()
+	{
+		
+	}
+	
+}
